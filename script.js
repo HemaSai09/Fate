518219b2bdb2a2e5b0401cffcfad219f339143dc
HemaSai09 <102684800+HemaSai09@users.noreply.github.com>
2024-03-21 12:29:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,36 @@ const video = document.getElementById('cameraFeed');
 
 // Open the camera modal when the camera button is clicked
 cameraBtn.addEventListener('click', () => {
-  modal.style.display = 'block';
-  openFullscreen(video);
+  openCamera();
 });
 
 // Close the camera modal when the close button is clicked
 closeBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
+  closeCamera();
 });
 
-// Open the camera in fullscreen mode
-function openFullscreen(element) {
-  if (element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if (element.mozRequestFullScreen) { /* Firefox */
-    element.mozRequestFullScreen();
-  } else if (element.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-    element.webkitRequestFullscreen();
-  } else if (element.msRequestFullscreen) { /* IE/Edge */
-    element.msRequestFullscreen();
-  }
+// Open the camera
+function openCamera() {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+      video.srcObject = stream;
+      modal.style.display = 'block';
+    })
+    .catch(err => {
+      console.error('Error accessing camera:', err);
+      alert('Error accessing camera. Please make sure your camera is connected and accessible.');
+    });
+}
+
+// Close the camera
+function closeCamera() {
+  const stream = video.srcObject;
+  const tracks = stream.getTracks();
+
+  tracks.forEach(track => {
+    track.stop();
+  });
+
+  video.srcObject = null;
+  modal.style.display = 'none';
 }
